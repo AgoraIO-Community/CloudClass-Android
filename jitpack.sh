a41ffdb7a856f8be4c42e9af7bd6996c497b50c2
陈镜吾 <chenjingwu@agora.io>
2021-01-19 17:51:23 +0800

    1: fix bug in jitpack.sh

--- jitpack.sh
+++ jitpack.sh
@@ -22,7 +22,7 @@ else
     echo "-----git commit FAILED!!!----"
     exit 0
 fi
-git push origin android-sdk:android-sdk
+git push origin aPaas:aPaas
 # shellcheck disable=SC2181
 if [ $? -eq 0 ]; then
     echo "-----push code success!---"
