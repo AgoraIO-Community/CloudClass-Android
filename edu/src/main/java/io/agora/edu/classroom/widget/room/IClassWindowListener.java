8c11b0719b4a6e8876734396a63ff5c310fcff79
brianxcli <neteasy1210@gmail.com>
2021-01-29 12:04:22 +0800

    repush for class room container

---
+++ edu/src/main/java/io/agora/edu/classroom/widget/room/IClassWindowListener.java
@@ -0,0 +1,11 @@
+package io.agora.edu.classroom.widget.room;
+
+public interface IClassWindowListener {
+    void onRefreshClassWindow();
+
+    void onCustomerService();
+
+    void onSwitchCamera();
+
+    void onLeaveRoom();
+}
