131c32e468163719e96348b991a519c4b76d2354
Felix He <fenggit@163.com>
2025-06-12 11:28:28 +0800

    Delete open-cloudclass-android directory

--- open-cloudclass-android/AgoraEduUIKit/src/main/java/io/agora/agoraeduuikit/impl/users/Roster.kt
+++
@@ -1,9 +0,0 @@
-package io.agora.agoraeduuikit.impl.users
-
-enum class RosterType(private val value: Int) {
-    SmallClass(0), LargeClass(1);
-
-    fun value(): Int {
-        return this.value
-    }
-}
--- open-cloudclass-android/AgoraEduUIKit/src/main/java/io/agora/agoraeduuikit/interfaces/listeners/IAgoraUIVideoListener.kt
+++
@@ -1,8 +0,0 @@
-package io.agora.agoraeduuikit.interfaces.listeners
-
-import android.view.ViewGroup
-import io.agora.agoraeduuikit.provider.AgoraUIUserDetailInfo
-
-interface IAgoraUIVideoListener {
-    fun onRendererContainer(viewGroup: ViewGroup?, info: AgoraUIUserDetailInfo)
-}
